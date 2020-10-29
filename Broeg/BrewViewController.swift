aacb8691fa6b2ac3c1c57a85f17daaf0f6e4dddb
Anders Brandt <s185016@student.dtu.dk>
2020-10-29 11:48:29 +0100

    Created brewViewController, and added two buttons to it

---
+++ Broeg/BrewViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BrewViewController.swift
+//  Broeg
+//
+//  Created by Anders Brandt on 29/10/2020.
+//
+
+import UIKit
+
+class BrewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func Brew(_ sender: UIButton) {
+        
+    }
+    
+
+    @IBAction func Favorites(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
