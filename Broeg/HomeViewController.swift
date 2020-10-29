d90e1256e360fe06329b5ed531579bf3100fbec6
Kjaerr97 <s185466@student.dtu.dk>
2020-10-29 12:04:23 +0100

    HomeViewController
    Rounded buttons

---
+++ Broeg/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Broeg
+//
+//  Created by Rasmus Kjær Mortensen on 29/10/2020.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func btnBroeg(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func btnRens(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func btnHistorik(_ sender: UIButton) {
+    }
+    
+    
+    
+
+}
+
