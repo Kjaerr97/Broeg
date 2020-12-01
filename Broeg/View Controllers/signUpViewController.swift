e2dc08b20f4f00c801490129919e5a2c1f00b185
Anders Brandt <s185016@student.dtu.dk>
2020-12-01 12:06:10 +0100

    merged the storyboard of home branch and login branch + fixed signup navigation

--- Broeg/View Controllers/signUpViewController.swift
+++ Broeg/View Controllers/signUpViewController.swift
@@ -132,9 +132,9 @@ class signUpViewController: UIViewController {
     // youtube link: https://www.youtube.com/watch?v=1HN7usMROt8&t=1894s&ab_channel=CodeWithChris
     // from 1:10:05
     func transitionToHome() {
-        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        let navigationController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.UINavigationController) as? UINavigationController
         
-        view.window?.rootViewController = homeViewController
-        view.window?.makeKeyAndVisible()
+        self.view.window?.rootViewController = navigationController
+        self.view.window?.makeKeyAndVisible()
     }
 }
