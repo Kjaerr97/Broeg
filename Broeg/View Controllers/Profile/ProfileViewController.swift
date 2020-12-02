dd0154c93f7b33da43864ac2056c9e74b46e442f
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2020-12-02 18:17:37 +0100

    New groups

--- Broeg/View Controllers/Profile/ProfileViewController.swift
+++ Broeg/View Controllers/Profile/ProfileViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import Firebase
 import FirebaseAuth
-import FirebaseDatabase
 
 class ProfileViewController: UIViewController, firstNameDelegate, lastNameDelegate, emailDelegate {
     
